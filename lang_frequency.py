0218f59541e0b7bbe5eb568a7eca22d00e6f3719
4pydev <4pydev@gmail.com>
2017-11-13 17:39:14 +0300

    Updated lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -9,20 +9,21 @@ def load_data(path_to_file):
     return init_text
 
 
-def get_most_frequent_words(init_text):
+def get_most_frequent_words(init_text, number_of_words):
     words_list = re.findall(r'\w+', init_text.lower())
-    number_of_words = 10
-    ten_most_frequent_words = Counter(words_list).most_common(number_of_words)
-    for word in ten_most_frequent_words:
-        print(str(word[0]) + " - " + str(word[1]))
+    most_frequent_words = Counter(words_list).most_common(number_of_words)
+    return most_frequent_words
 
 
 if __name__ == '__main__':
+    number_of_words = 10
     try:
         path_to_file = sys.argv[1]
         simple_text = load_data(path_to_file)
-        get_most_frequent_words(simple_text)
-    except IndexError as ind_err:
+        for word, frequency in get_most_frequent_words(simple_text,
+                                                       number_of_words):
+            print(str(word) + " - " + str(frequency))
+    except IndexError as index_error:
         print("You must enter the path to file.")
-    except OSError as fnf_err:
+    except OSError as file_not_found_error:
         print("File not found. You must enter the valid path to file.")
